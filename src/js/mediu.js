148204c1228b42197e22ed7f35b0195d99559995
wutaosusan <wutaosusan@didichuxing.com>
2016-12-16 18:24:59 +0800

    灰度

--- src/js/mediu.js
+++ src/js/mediu.js
@@ -44,17 +44,32 @@ image.onload = function(){
     context.drawImage(image,200,899,100,100,0,0,100,100);
 
     //访问像素值
-    var imageData = context.getImageData(100,100,8,8); //8*8栅格
+    var imageData = context.getImageData(100,100,200,200); //200*200栅格
     var width = imageData.width,
         height = imageData.height,
-        pixel = imageData.data; //CanvasPixelArray
+        pixels = imageData.data; //CanvasPixelArray
+    var numPixels = imageData.width*imageData.height;
 
-    var x = 2,y = 2;
+    for(var i = 0; i < numPixels; i++){
+        //反转
+        //pixels[i*4] = 255 - pixels[i*4];
+        //pixels[i*4+1] = 255 - pixels[i*4+1];
+        //pixels[i*4+2] = 255 - pixels[i*4+2];
 
-    var pixelRed = ((y-1)*width*4) + ((x-1)*4);
-    var pixelGreen = pixelRed + 1,
-        pixelBlue = pixelRed + 2,
-        pixelAlpha = pixelRed + 3;
+        //灰度
+        var average = (pixels[i*4] + pixels[i*4+1] + pixels[i*4+2])/3;
+        pixels[i*4] = average;
+        pixels[i*4+1] = average;
+        pixels[i*4+2] = average;
+    }
+    context.putImageData(imageData,400,100);
+
+    //var x = 2,y = 2;
+    //
+    //var pixelRed = ((y-1)*width*4) + ((x-1)*4);
+    //var pixelGreen = pixelRed + 1,
+    //    pixelBlue = pixelRed + 2,
+    //    pixelAlpha = pixelRed + 3;
 }
 
 var imageData = context.createImageData(100,100);
@@ -70,10 +85,31 @@ for(var i = 0; i < numPixels; i++){
 context.putImageData(imageData,0,0);
 
 //马赛克
-var imageData1 = context.createImageData(500,500);
-var pixels = imageData.data;
+var imageData1 = context.createImageData(100,100);
+var pixels1 = imageData1.data;
 //马赛克块的个数
 var numTitleRows = 4;
 var numTitleCols = 4;
 
 //每个块的尺寸
+var titleWidth = imageData1.width/numTitleCols;
+var titleHeight = imageData1.height/numTitleRows;
+for(var r = 0; r < numTitleRows; r++){
+    for(var c = 0; c < numTitleCols; c++){
+        var red = Math.floor(Math.random()*255),
+            green = Math.floor(Math.random()*255),
+            blue = Math.floor(Math.random()*255);
+        for(var tr = 0; tr < titleHeight; tr++){
+            for(var tc = 0; tc < titleWidth; tc++){
+                var trueX = (c*titleWidth) + tc;
+                var trueY = (r*titleHeight) + tr;
+                var pos = (trueY*(imageData1.width*4))+(trueX*4);
+                pixels1[pos] = red;
+                pixels1[pos+1] = green;
+                pixels1[pos+2] = blue;
+                pixels1[pos+3] = 255;
+            }
+        }
+    }
+}
+context.putImageData(imageData1,300,300);
